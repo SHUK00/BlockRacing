ca4c818ae3865741abbcead7b77498feb2122e05
luni14 <te14.niklas.lundstrom@umea.nti.se>
2017-03-03 10:36:43 +0100

    grunden till skelettet

--- src/view.java
+++ src/view.java
@@ -1,9 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by shse14 on 2017-02-24.
  */
-public class view {
+public class view extends JPanel {
+    track track;
     public view(){
-        
+        track = new track();
     }
     public void viewTrack(){
 
